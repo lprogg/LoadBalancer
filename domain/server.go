383886feb38639e8fe18fb5d83266597bdfe6319
lprogg <lprogg>
2022-12-19 09:54:30 +0200

    Add metadata to Weighted Round Robin

--- domain/server.go
+++ domain/server.go
@@ -1,8 +1,10 @@
 package domain
 
 import (
+	"fmt"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
 )
 
 type Service struct {
@@ -15,9 +17,31 @@ type Service struct {
 type Server struct {
 	URL *url.URL
 	Proxy *httputil.ReverseProxy
+	Metadata map[string]string
 }
 
 type Replica struct {
 	URL string `yml:"url"`
 	Metadata map[string]string `yml:"metadata"`
+}
+
+func (s *Server) GetMetadataOrDefaultString(key, defaultValue string) string {
+	value, ok := s.Metadata[key]
+
+	if !ok {
+		return defaultValue
+	}
+
+	return value
+}
+
+func (s *Server) GetMetadataOrDefaultInt(key string, defaultValue int) int {
+	stringValue := s.GetMetadataOrDefaultString(key, fmt.Sprintf("%d", defaultValue))
+	intValue, err := strconv.Atoi(stringValue)
+
+	if err != nil {
+		return defaultValue
+	}
+
+	return intValue
 }
--- strategy/api.go
+++ strategy/api.go
@@ -2,12 +2,16 @@ package strategy
 
 import (
 	"fmt"
+	"sync"
 	"sync/atomic"
 
 	"github.com/lprogg/LoadBalancer/domain"
 )
 
-const RoundRobinStrategy = "RoundRobin"
+const (
+	RoundRobinStrategy = "RoundRobin"
+	WeightedRoundRobinStrategy = "WeightedRoundRobinStrategy"
+)
 
 var strategies map[string]func() BalancingStrategy
 
@@ -17,6 +21,12 @@ type BalancingStrategy interface {
 
 type RoundRobin struct {
 	current uint64
+}
+
+type WeightedRoundRobin struct {
+	mutex sync.Mutex
+	count []int
+	current int
 }
 
 func (r *RoundRobin) NextServer(servers []*domain.Server) (*domain.Server, error) {
@@ -29,10 +39,39 @@ func (r *RoundRobin) NextServer(servers []*domain.Server) (*domain.Server, error
 	return selected, nil
 }
 
+func (r *WeightedRoundRobin) NextServer(servers []*domain.Server) (*domain.Server, error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if r.count == nil {
+		r.count = make([]int, len(servers))
+		r.current = 0
+	}
+
+	capacity := servers[r.current].GetMetadataOrDefaultInt("weight", 1)
+	
+	if r.count[r.current] <= capacity {
+		r.count[r.current] += 1
+		fmt.Printf("Strategy selected server: '%s'\n", servers[r.current].URL.Host)
+		return servers[r.current], nil
+	}
+
+	r.count[r.current] = 0
+	r.current = (r.current + 1) % len(servers)
+	fmt.Printf("Strategy selected server: '%s'\n", servers[r.current].URL.Host)
+	
+	return servers[r.current], nil
+}
+
 func init() {
 	strategies = make(map[string]func() BalancingStrategy)
+	
 	strategies[RoundRobinStrategy] = func() BalancingStrategy {
 		return &RoundRobin{current: 0}
+	}
+
+	strategies[WeightedRoundRobinStrategy] = func() BalancingStrategy {
+		return &WeightedRoundRobin{mutex: sync.Mutex{}}
 	}
 }
 
